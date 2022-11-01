38a406a4744de7e412675447040385263c2132bf
Casey Ydenberg <hi@caydenberg.io>
2022-11-01 16:43:07 -0700

    WIP animation works ok time to focus on other things

--- src/MainChart/MainChart.tsx
+++ src/MainChart/MainChart.tsx
@@ -12,6 +12,7 @@ interface Props {
   yAxisLabel: string;
   highlight: (key: string) => void;
   timelineData?: [string, number][];
+  overlayOpacity: number;
 }
 
 const interpolateData = (prev: PlanarDataPoint[], next: PlanarDataPoint[]) => {
@@ -39,13 +40,25 @@ const interpolateData = (prev: PlanarDataPoint[], next: PlanarDataPoint[]) => {
   };
 };
 
-const useInterpolatedEffect = createUseInterpolatedEffect(
-  (prev: Props, next: Props) => {
-    console.log(prev.planarData, next.planarData);
+const useInterpolatedEffect = createUseInterpolatedEffect<Props>(
+  (prev, next) => {
     if (prev.yAxisLabel !== next.yAxisLabel) {
       return (progress: number) => ({
         ...next,
         planarData: interpolateData(prev.planarData, next.planarData)(progress),
+      });
+    }
+    if (prev.highlighted && !next.highlighted) {
+      return (progress: number) => ({
+        ...next,
+        overlayOpacity: 1 - progress,
+        timelineData: progress < 1 ? prev.timelineData : undefined,
+      });
+    }
+    if (prev.highlighted !== next.highlighted) {
+      return (progress: number) => ({
+        ...next,
+        overlayOpacity: progress,
       });
     }
     return next;
@@ -53,17 +66,52 @@ const useInterpolatedEffect = createUseInterpolatedEffect(
 );
 
 export const MainChart: React.FC<Props> = (props) => {
-  const [hovered, setHovered] = useState('');
-
   const [
-    { planarData, highlighted, yAxisLabel, highlight, timelineData },
+    { planarData, yAxisLabel, overlayOpacity, timelineData },
     isAnimating,
   ] = useInterpolatedEffect(props);
 
-  const view = getViewbox(planarData, timelineData);
-  if (!view) {
+  return (
+    <AnimatedChart
+      {...props}
+      planarData={planarData}
+      timelineData={timelineData}
+      yAxisLabel={yAxisLabel}
+      isAnimating={isAnimating}
+      overlayOpacity={overlayOpacity}
+    />
+  );
+};
+
+MainChart.defaultProps = {
+  overlayOpacity: 1,
+};
+
+interface MainChartAnimatedProps extends Props {
+  overlayOpacity: number;
+  isAnimating: boolean;
+}
+
+const AnimatedChart: React.FC<MainChartAnimatedProps> = ({
+  planarData,
+  highlighted,
+  yAxisLabel,
+  highlight,
+  timelineData,
+  isAnimating,
+  overlayOpacity,
+}) => {
+  const [hovered, setHovered] = useState('');
+
+  const basicView = getViewbox(planarData, timelineData);
+
+  if (!basicView) {
     return null;
   }
+
+  const view = isAnimating
+    ? getViewbox(planarData)!.interpolate(basicView, overlayOpacity)
+    : basicView;
 
   const highlightedData = planarData.find(
     (point) => point.compoundKey === highlighted,
@@ -129,6 +177,7 @@ export const MainChart: React.FC<Props> = (props) => {
           planarData={highlightedData}
           compoundKey={highlighted}
           view={view}
+          opacity={overlayOpacity}
         />
       </Chart>
     </div>
